6f1310820b407cf961b85f826300854a90a04bdc
MicLois <michela.e.langlois@gmail.com>
2026-05-12 20:50:42 -0400

    Update app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,18 +20,3 @@ function App() {
 }
 
 export default App;
-
-import { useState } from "react";
-
-export default function Counter() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>
-        Increment
-      </button>
-    </div>
-  );
-}
