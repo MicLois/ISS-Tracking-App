76b42a8fc52bf482ead8b70c125eaac1a197d7bd
MicLois <michela.e.langlois@gmail.com>
2026-05-16 13:16:30 -0400

    Updated ISSTracker Api (Working)

--- client/src/api/ issApi.js
+++ client/src/api/ issApi.js
@@ -1,5 +1,4 @@
 export async function getISSLocation() {
-  const response = await fetch("https://iss-tracking-app-client.onrender.com/");
   const response = await fetch("https://api.open-notify.org/iss-now.json");
 
   if (!response.ok) {
