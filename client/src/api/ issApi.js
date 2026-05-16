d6a203d9da325e574d0109ffb890cb6683ebc458
MicLois <michela.e.langlois@gmail.com>
2026-05-16 12:58:35 -0400

    Update  issApi.js

--- client/src/api/ issApi.js
+++ client/src/api/ issApi.js
@@ -1,7 +1,11 @@
 export async function getISSLocation() {
+<<<<<<< HEAD
   const response = await fetch("https://iss-tracking-app-client.onrender.com/");
   //const response = await fetch("https://your-backend.onrender.com/iss");
   //const response = await fetch("http://api.open-notify.org/iss-now.json");
+=======
+  const response = await fetch("http://api.open-notify.org/iss-now.json");
+>>>>>>> parent of be295c5 (Update  issApi.js)
 
   if (!response.ok) {
     throw new Error("Failed to fetch ISS data");
